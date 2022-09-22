abeef14834982b79e6d463246bcf19eade8b1ca4
Victor <44535997+VictorGamer072YT@users.noreply.github.com>
2022-09-22 13:02:44 -0300

    Skips logging the Readme and Redist folders
    It also includes code to print the current exe version to the txt, but at the moment it prints far too much info.

--- BugReport/Program.cs
+++ BugReport/Program.cs
@@ -2,23 +2,27 @@
 using System.IO;
 using System.Text;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
 
 namespace BugReport
 {
     internal class Program
     {
-        private static StringBuilder retString = new StringBuilder();
+        private static readonly StringBuilder retString = new StringBuilder();
         private static void Main(string[] args)
         {
-
             if (!File.Exists("GTA5.exe"))
             {
                 MessageBox.Show("Please place this .exe file in your Grand Theft Auto V directory, then try again.", "GTA5.exe not found", MessageBoxButtons.OK, MessageBoxIcon.Error); //Somehow our users are stupid enough to not understand simple instructions.
-                Environment.Exit(0); //Application.Exit doesn't work for some reason.
+                Environment.Exit(0); //Application.Exit didn't work for some reason.
             }
 
+            var verInfo = FileVersionInfo.GetVersionInfo(Path.Combine(Environment.CurrentDirectory, "GTA5.exe"));
 
+            //retString.AppendLine($"———— GTA 5 EXE Data ————\n{verInfo}");
+            //retString.AppendLine();
 
             try
             {
@@ -69,10 +73,9 @@ namespace BugReport
             }
 
             retString.AppendLine("———— FILES ————");
-            foreach (string file in Directory.GetFiles(".\\", "*", SearchOption.AllDirectories))
+            foreach (string file in Directory.GetFiles(".\\", "*", SearchOption.AllDirectories).Where(s => !(s.Contains("\\Redistributables\\") || s.Contains("\\ReadMe\\") || s.Contains("\\_Installer\\") || s.Contains("\\CommonRedist\\") || s.Contains("\\.egstore\\"))))
             retString.AppendLine(file.Replace(".\\", ""));
             retString.AppendLine();
-            // TO-DO: Stop logging the following folders: ReadMe, Redistribuitables, _Installer, CommonRedist
             File.WriteAllText(string.Format(".\\BUGREPORT_{0:yyyy-MM-dd_HH-mm-ss}.txt", DateTime.Now), retString.ToString());
         }
     }
