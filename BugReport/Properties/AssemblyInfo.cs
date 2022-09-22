8de6212060ebc1813606b881658f88b10b6ce738
Victor <44535997+VictorGamer072YT@users.noreply.github.com>
2022-09-22 13:02:54 -0300

    Bump version to 1.3.0

--- BugReport/Properties/AssemblyInfo.cs
+++ BugReport/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("No Rights Reserved")]
 [assembly: ComVisible(false)]
 [assembly: Guid("30401207-c490-4c04-9f52-bc47c559e1e8")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
